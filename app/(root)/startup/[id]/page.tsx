ad4e2e349e5d36ef4993452534eeddbb5dc4aa3e
eng-kerolos-adel <eng.kerolos.adel.eleshaa@gmail.com>
2025-09-09 17:39:07 +0300

    first commit

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -19,7 +19,7 @@ const md = markdownit();
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const [post, { select: editorPosts }] = await Promise.all([
+  const [post, playlist] = await Promise.all([
     client.fetch(STARTUP_BY_ID_QUERY, { id }),
     client.fetch(PLAYLIST_BY_SLUG_QUERY, {
       slug: "editor-picks-new",
@@ -27,6 +27,8 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   ]);
 
   if (!post) return notFound();
+
+  const editorPosts = playlist?.select ?? [];
 
   const parsedContent = md.render(post?.pitch || "");
 
@@ -84,7 +86,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {editorPosts?.length > 0 && (
+        {editorPosts.length > 0 && (
           <div className="max-w-4xl mx-auto">
             <p className="text-30-semibold">Editor Picks</p>
 
