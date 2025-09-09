eb149a520b84aa19415ecced3d9860752b460c2e
eng-kerolos-adel <eng.kerolos.adel.eleshaa@gmail.com>
2025-09-09 16:21:55 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   transpilePackages: ["sanity-plugin-markdown"],
@@ -27,8 +19,6 @@ const nextConfig: NextConfig = {
     ],
   },
   devIndicators: {
-    appIsrStatus: true,
-    buildActivity: true,
     buildActivityPosition: "bottom-right",
   },
 };
